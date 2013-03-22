5ee0003194c9cb804e37ce3d68cb30ea3651fa26
Kevin Gaddy <kevin.gaddy@gmail.com>
2013-03-22 15:38:25 -0500

    init
    Signed-off-by: Kevin Gaddy <kevin.gaddy@gmail.com>

---
+++ HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by kgaddy on 3/22/13.
+//  Copyright (c) 2013 com.kgaddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
